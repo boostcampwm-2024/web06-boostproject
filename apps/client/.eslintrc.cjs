c51fa4e1e70fe2ba13706221010a73dd8e01e424
PMtHk <zooby88@gmail.com>
2024-11-05 18:46:11 +0900

    chore: client lint 규칙 설정

---
+++ apps/client/.eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'airbnb', // or airbnb-base
+		'plugin:react/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+		'prettier/prettier': ['error', { endOfLine: 'auto' }],
+		'no-shadow': 0,
+	},
+	overrides: [
+		{
+			files: ['vite.config.ts'],
+			rules: {
+				'import/no-extraneous-dependencies': 'off',
+			},
+		},
+	],
+};

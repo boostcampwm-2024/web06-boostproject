5e951a2cf238860da97cce768f6fdf0bf495a365
iam454 <realseogy@gmail.com>
2024-11-06 23:34:03 +0900

    feat: section 컴포넌트 생성

---
+++ apps/client/src/components/ui/section.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+const Section = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+	({ className, ...props }, ref) => (
+		<div
+			ref={ref}
+			className={cn('bg-card text-card-foreground rounded-xl border shadow', className)}
+			{...props}
+		/>
+	)
+);
+Section.displayName = 'Section';
+
+const SectionHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+	({ className, ...props }, ref) => (
+		<div ref={ref} className={cn('flex flex-col space-y-1.5 p-4', className)} {...props} />
+	)
+);
+SectionHeader.displayName = 'SectionHeader';
+
+const SectionTitle = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+	({ className, ...props }, ref) => (
+		<div
+			ref={ref}
+			className={cn('font-semibold leading-none tracking-tight', className)}
+			{...props}
+		/>
+	)
+);
+SectionTitle.displayName = 'SectionTitle';
+
+const SectionDescription = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+	({ className, ...props }, ref) => (
+		<div ref={ref} className={cn('text-muted-foreground text-sm', className)} {...props} />
+	)
+);
+SectionDescription.displayName = 'SectionDescription';
+
+const SectionContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+	({ className, ...props }, ref) => (
+		<div ref={ref} className={cn('p-2 pt-0', className)} {...props} />
+	)
+);
+SectionContent.displayName = 'SectionContent';
+
+const SectionFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+	({ className, ...props }, ref) => (
+		<div ref={ref} className={cn('flex items-center p-4', className)} {...props} />
+	)
+);
+SectionFooter.displayName = 'SectionFooter';
+
+export { Section, SectionHeader, SectionFooter, SectionTitle, SectionDescription, SectionContent };

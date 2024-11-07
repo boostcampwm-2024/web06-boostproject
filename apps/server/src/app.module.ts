48283325ee0de09189810cbbe1039d164b792e0d
yang <hc1489@naver.com>
2024-11-07 21:13:42 +0900

    chore: TypeORM, MySQL 연동

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeormConfig } from '../config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRootAsync({
+			useClass: TypeormConfig,
+			dataSourceFactory: async (options: DataSourceOptions) => {
+				return new DataSource(options).initialize();
+			},
+		}),
+		TaskModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })

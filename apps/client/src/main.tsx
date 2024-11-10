bcad6cb88b6edacc6b0dac5c35e8ea744814b83b
iam454 <realseogy@gmail.com>
2024-11-10 15:44:32 +0900

    feat: tanstack router 적용

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,23 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { routeTree } from './routeTree.gen';
 import './global.css';
-
-const router = createRouter({ routeTree });
-
-declare module '@tanstack/react-router' {
-	interface Register {
-		router: typeof router;
-	}
-}
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
 	const root = createRoot(rootElement);
 	root.render(
 		<StrictMode>
-			<RouterProvider router={router} />
+			<App />
 		</StrictMode>
 	);
 }
--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -1,21 +1,9 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
-			<div className="flex gap-2 p-2">
-				<Link to="/" className="[&.active]:font-bold">
-					Harmony
-				</Link>
-				<Link to="/login" className="[&.active]:font-bold">
-					Login
-				</Link>
-				<Link to="/join" className="[&.active]:font-bold">
-					Join
-				</Link>
-			</div>
-			<hr />
 			<Outlet />
 			<TanStackRouterDevtools />
 		</>

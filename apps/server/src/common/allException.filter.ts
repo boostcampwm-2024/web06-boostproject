4d3dfad0493b42db6d102475a6e9fc02baaee641
jjeonghak <rlawjdgks3185@khu.ac.kr>
2024-11-10 14:52:32 +0900

    chore: 오류 응답 포멧 설정

---
+++ apps/server/src/common/allException.filter.ts
@@ -0,0 +1,22 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, Logger } from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+	private readonly logger = new Logger(AllExceptionsFilter.name);
+
+	catch(exception: any, host: ArgumentsHost) {
+		const context = host.switchToHttp();
+		const request = context.getRequest();
+		const response = context.getResponse();
+		const { method, url, ip } = request;
+		const status = exception instanceof HttpException ? exception.getStatus() : 500;
+		const message = exception.message || exception;
+
+		this.logger.error(`[RESPONSE] ${method} ${url} ${ip} - ${status} ${message}`);
+		response.status(status).json({
+			statusCode: status,
+			message,
+			success: false,
+		});
+	}
+}

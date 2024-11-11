7a82a3bc6fa09aca923d79c906177a70355bbb8c
jjeonghak <rlawjdgks3185@khu.ac.kr>
2024-11-11 22:58:25 +0900

    feat: 프로젝트 멤버 조회 API 구현

--- apps/server/src/project/service/project.service.ts
+++ apps/server/src/project/service/project.service.ts
@@ -8,6 +8,7 @@ import { ProjectRole } from '../enum/project-role.enum';
 import { CreateProjectResponse } from '../dto/create-project-response.dto';
 import { UserProjectsResponse } from '../dto/user-projects-response.dto';
 import { Account } from '@/account/entity/account.entity';
+import { ProjectContributorsResponse } from '../dto/project-contributors-response-dto';
 
 @Injectable()
 export class ProjectService {
@@ -34,6 +35,22 @@ export class ProjectService {
 				});
 			}
 		);
+	}
+
+	async getContributors(userId: number, projectId: number) {
+		const userContributor = await this.contributorRepository.findOneBy({ userId, projectId });
+		if (!userContributor) {
+			throw new NotFoundException('Does not found user contributor or project');
+		}
+		const result = await this.contributorRepository
+			.createQueryBuilder('c')
+			.leftJoin('account', 'a', 'c.userId = a.id')
+			.where('c.projectId = :projectId', { projectId })
+			.addSelect(['a.id, a.username, c.role'])
+			.getRawMany();
+		return result.map((record: { id: number; username: string; role: ContributorStatus }) => {
+			return new ProjectContributorsResponse(record.id, record.username, record.role);
+		});
 	}
 
 	async create(userId: number, title: string) {

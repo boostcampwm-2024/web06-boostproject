9b8494b892d1b24ff0816b6ff732534b58917d67
PMtHk <zooby88@gmail.com>
2024-11-11 15:04:24 +0900

    feat: Logo 컴포넌트 작성

---
+++ apps/client/src/components/logo/index.ts
@@ -0,0 +1,5 @@
+import Github from '@/components/logo/GitHub';
+import Harmony from '@/components/logo/Harmony';
+import HarmonyIcon from '@/components/logo/HarmonyIcon';
+
+export { Harmony, HarmonyIcon, Github };

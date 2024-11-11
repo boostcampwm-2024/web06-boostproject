b62f3085ff5e0cf97dc4cdb5407be4ce860e9363
jjeonghak <rlawjdgks3185@khu.ac.kr>
2024-11-11 21:09:15 +0900

    feat: 프로젝트 목록 조회 API 구현

---
+++ apps/server/src/project/dto/user-projects-response.dto.ts
@@ -0,0 +1,12 @@
+import { ContributorStatus } from '../enum/contributor-status.enum';
+
+export class UserProjectsResponse {
+	role: ContributorStatus;
+
+	project: { id: number; title: string; createdAt: Date };
+
+	constructor(role: ContributorStatus, project: { id: number; title: string; createdAt: Date }) {
+		this.role = role;
+		this.project = project;
+	}
+}
--- apps/server/src/project/service/project.service.ts
+++ apps/server/src/project/service/project.service.ts
@@ -6,6 +6,7 @@ import { Contributor } from '../entity/contributor.entity';
 import { ContributorStatus } from '../enum/contributor-status.enum';
 import { ProjectRole } from '../enum/project-role.enum';
 import { CreateProjectResponse } from '../dto/create-project-response.dto';
+import { UserProjectsResponse } from '../dto/user-projects-response.dto';
 
 @Injectable()
 export class ProjectService {
@@ -13,6 +14,25 @@ export class ProjectService {
 		@InjectRepository(Project) private projectRepository: Repository<Project>,
 		@InjectRepository(Contributor) private contributorRepository: Repository<Contributor>
 	) {}
+
+	async getUserProjects(userId: number) {
+		const result = await this.contributorRepository
+			.createQueryBuilder('c')
+			.leftJoin('project', 'p', 'c.projectId = p.id')
+			.where('c.userId = :userId', { userId })
+			.andWhere('c.status = :status', { status: ContributorStatus.ACCEPTED })
+			.addSelect(['p.title', 'p.createdAt'])
+			.getRawMany();
+		return result.map(
+			(record: { p_id: number; p_title: string; p_createdAt: Date; c_role: ContributorStatus }) => {
+				return new UserProjectsResponse(record.c_role, {
+					id: record.p_id,
+					title: record.p_title,
+					createdAt: record.p_createdAt,
+				});
+			}
+		);
+	}
 
 	async create(userId: number, title: string) {
 		const project = await this.projectRepository.save({ title });

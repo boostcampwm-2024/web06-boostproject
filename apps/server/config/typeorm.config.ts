89d2a5d0505914983cf6b1bd7e92da56963148ca
jjeonghak <rlawjdgks3185@khu.ac.kr>
2024-11-11 15:05:40 +0900

    refactor: 린팅 수정

--- apps/server/config/typeorm.config.ts
+++ apps/server/config/typeorm.config.ts
@@ -1,8 +1,8 @@
-import { Section } from '@/task/domain/section.entity';
-import { Task } from '@/task/domain/task.entity';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Task } from '@/task/domain/task.entity';
+import { Section } from '@/task/domain/section.entity';
 import { Account } from '@/account/entity/account.entity';
 
 @Injectable()
@@ -17,11 +17,7 @@ export class TypeormConfig implements TypeOrmOptionsFactory {
 			username: this.configService.get<string>('DATABASE_USER'),
 			password: this.configService.get<string>('DATABASE_PASSWORD'),
 			database: this.configService.get<string>('DATABASE_NAME'),
-			entities: [
-				Task, 
-				Section, 
-				Account
-			],
+			entities: [Task, Section, Account],
 			synchronize: true,
 			ssl: {
 				rejectUnauthorized: false,
--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,14 +2,13 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { TaskModule } from './task/task.module';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeormConfig } from '../config/typeorm.config';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { HttpLoggingInterceptor } from './common/httpLog.Interceptor';
 import { AllExceptionsFilter } from './common/allException.filter';
-import { APP_PIPE } from '@nestjs/core';
 import { AccountModule } from './account/account.module';
 
 @Module({

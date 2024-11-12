155550ba5721b356e955b2d89a612f9a02f59a5b
PMtHk <zooby88@gmail.com>
2024-11-12 18:23:27 +0900

    feat: QueryClientProvider 적용

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,26 +1,31 @@
 import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { routeTree } from './routeTree.gen';
 import { AuthProvider, useAuth } from './contexts/authContext';
 
 const router = createRouter({ routeTree, context: { auth: undefined! } });
 
 declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
+	interface Register {
+		router: typeof router;
+	}
 }
 
 function InnerApp() {
-  const auth = useAuth();
-  return <RouterProvider router={router} context={{ auth }} />;
+	const auth = useAuth();
+	return <RouterProvider router={router} context={{ auth }} />;
 }
 
+const queryClient = new QueryClient();
+
 function App() {
-  return (
-    <AuthProvider>
-      <InnerApp />
-    </AuthProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<AuthProvider>
+				<InnerApp />
+			</AuthProvider>
+		</QueryClientProvider>
+	);
 }
 
 export default App;

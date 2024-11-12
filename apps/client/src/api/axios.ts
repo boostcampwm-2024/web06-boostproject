b17816537d95a779aa3bc6493ae85778faa9becf
PMtHk <zooby88@gmail.com>
2024-11-12 15:46:53 +0900

    feat: axios 인스턴스 생성

---
+++ apps/client/src/api/axios.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const { VITE_API_SERVER_URL } = import.meta.env;
+
+const api = axios.create({
+	baseURL: VITE_API_SERVER_URL,
+	headers: {
+		'Content-Type': 'application/json',
+	},
+});
+
+api.interceptors.request.use(
+	(config) => {
+		const newConfig = { ...config };
+
+		const accessToken = ''; // accessToken 은 store 에서 가져오기
+		if (accessToken) {
+			newConfig.headers.Authorization = `Bearer ${accessToken}`;
+		}
+
+		return newConfig;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
+export default api;

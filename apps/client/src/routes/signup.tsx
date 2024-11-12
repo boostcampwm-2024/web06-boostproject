70691fc6b12c13f1a50c0f0cc41225c52d5ee36d
iam454 <realseogy@gmail.com>
2024-11-12 12:49:01 +0900

    fix: 주엽님과의 작업 사항 합치기

--- apps/client/src/routes/signup.tsx
+++ apps/client/src/routes/signup.tsx
@@ -1,6 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router';
-import Signup from '@/pages/Signup.tsx';
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import Signup from '@/pages/Signup';
 
 export const Route = createFileRoute('/signup')({
+	beforeLoad: ({ context }) => {
+		if (context.auth.isAuthenticated) {
+			throw redirect({ to: '/account' });
+		}
+	},
 	component: Signup,
 });

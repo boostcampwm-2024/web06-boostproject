da0274ae594eada6fe308ec70b47e8b4f35b8b9c
PMtHk <zooby88@gmail.com>
2024-11-12 18:22:42 +0900

    fix: password_confirm 스키마 변경

--- apps/client/src/SignupSchema.ts
+++ apps/client/src/SignupSchema.ts
@@ -7,7 +7,7 @@ export const SignupSchema = z
 			.min(8, {
 				message: '아이디는 8자 이상이어야 합니다.',
 			})
-			.max(15, {
+			.max(20, {
 				message: '아이디는 15자 이하여야 합니다.',
 			}),
 
@@ -20,9 +20,9 @@ export const SignupSchema = z
 				message: '비밀번호는 15자 이하여야 합니다.',
 			}),
 
-		passwordConfirm: z.string().min(8, { message: '비밀번호 확인은 최소 8자 이상이어야 합니다.' }),
+		passwordConfirm: z.string(),
 	})
 	.refine((data) => data.password === data.passwordConfirm, {
-		message: '비밀번호가 일치하지 않습니다.',
+		message: '비밀번호 확인이 일치하지 않습니다.',
 		path: ['passwordConfirm'],
 	});

c4bce81aa6408935133655819fea54e76c025295
iam454 <realseogy@gmail.com>
2024-11-14 01:18:20 +0900

    fix: prettier 탭 이슈 수정

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -1,28 +1,28 @@
 import { Github, Harmony } from '@/components/logo';
 
 function Footer() {
-	return (
-		<footer className="flex h-[100px] w-full items-center justify-center bg-white p-5 text-gray-500 dark:bg-gray-800">
-			<div className="container mx-auto flex max-w-6xl flex-col items-center justify-between md:flex-row">
-				<div className="flex items-center space-x-4">
-					<div className="flex justify-center">
-						<Harmony />
-						<span className="pl-4">2024 Naver Boostcamp Project</span>
-					</div>
-				</div>
-				<div className="mt-4 flex space-x-4 md:mt-0">
-					<a
-						target="_blank"
-						rel="noreferrer"
-						href="https://github.com/boostcampwm-2024/web06-harmony"
-						className="hover:text-secondary"
-					>
-						<Github size={24} color="currentColor" />
-					</a>
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="flex h-[100px] w-full items-center justify-center bg-white p-5 text-gray-500 dark:bg-gray-800">
+      <div className="container mx-auto flex max-w-6xl flex-col items-center justify-between md:flex-row">
+        <div className="flex items-center space-x-4">
+          <div className="flex justify-center">
+            <Harmony />
+            <span className="pl-4">2024 Naver Boostcamp Project</span>
+          </div>
+        </div>
+        <div className="mt-4 flex space-x-4 md:mt-0">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/boostcampwm-2024/web06-harmony"
+            className="hover:text-secondary"
+          >
+            <Github size={24} color="currentColor" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- apps/client/src/contexts/authContext.tsx
+++ apps/client/src/contexts/authContext.tsx
@@ -1,59 +1,59 @@
 import { createContext, ReactNode, useCallback, useContext, useMemo, useState } from 'react';
 
 export interface AuthContext {
-	isAuthenticated: boolean;
-	login: (username: string, accessToken: string) => Promise<void>;
-	logout: () => Promise<void>;
-	username: string;
-	accessToken: string;
+  isAuthenticated: boolean;
+  login: (username: string, accessToken: string) => Promise<void>;
+  logout: () => Promise<void>;
+  username: string;
+  accessToken: string;
 }
 
 export const initialAuthContext: AuthContext = {
-	isAuthenticated: false,
-	login: async () => {},
-	logout: async () => {},
-	username: '',
-	accessToken: '',
+  isAuthenticated: false,
+  login: async () => {},
+  logout: async () => {},
+  username: '',
+  accessToken: '',
 };
 
 const AuthContext = createContext<AuthContext>(initialAuthContext);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-	const [username, setUsername] = useState<string>('');
-	const [accessToken, setAccessToken] = useState<string>('');
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>('');
+  const [accessToken, setAccessToken] = useState<string>('');
 
-	const login = useCallback(async (username: string, accessToken: string) => {
-		setIsAuthenticated(true);
-		setUsername(username);
-		setAccessToken(accessToken);
-	}, []);
+  const login = useCallback(async (username: string, accessToken: string) => {
+    setIsAuthenticated(true);
+    setUsername(username);
+    setAccessToken(accessToken);
+  }, []);
 
-	const logout = useCallback(async () => {
-		setIsAuthenticated(false);
-		setUsername('');
-		setAccessToken('');
-	}, []);
+  const logout = useCallback(async () => {
+    setIsAuthenticated(false);
+    setUsername('');
+    setAccessToken('');
+  }, []);
 
-	const value = useMemo(
-		() => ({
-			isAuthenticated,
-			login,
-			logout,
-			username,
-			accessToken,
-		}),
-		[isAuthenticated, login, logout, username, accessToken]
-	);
+  const value = useMemo(
+    () => ({
+      isAuthenticated,
+      login,
+      logout,
+      username,
+      accessToken,
+    }),
+    [isAuthenticated, login, logout, username, accessToken]
+  );
 
-	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 export const useAuth = () => {
-	const context = useContext(AuthContext);
-	if (!context) {
-		throw new Error('useAuth must be used within an AuthProvider');
-	}
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
 
-	return context;
+  return context;
 };

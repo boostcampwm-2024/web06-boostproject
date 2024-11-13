399513f272558f1ee41cfe7258d971ce1e9bbf82
jjeonghak <rlawjdgks3185@khu.ac.kr>
2024-11-13 22:07:24 +0900

    fix: 서버 엔티티 의존성 문제 해결

--- apps/server/src/project/project.module.ts
+++ apps/server/src/project/project.module.ts
@@ -5,9 +5,10 @@ import { ProjectController } from './controller/project.controller';
 import { Project } from './entity/project.entity';
 import { Contributor } from './entity/contributor.entity';
 import { Account } from '@/account/entity/account.entity';
+import { Task } from '@/task/domain/task.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project, Contributor, Account])],
+  imports: [TypeOrmModule.forFeature([Project, Contributor, Account, Task])],
   controllers: [ProjectController],
   providers: [ProjectService],
 })

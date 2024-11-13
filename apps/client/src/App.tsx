a36f0527b667a365943f001fae30d1a7b229d2df
iam454 <realseogy@gmail.com>
2024-11-14 01:17:42 +0900

    feat: 사용자 프로젝트 목록 조회 api 연결

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -3,29 +3,34 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { routeTree } from './routeTree.gen';
 import { AuthProvider, useAuth } from './contexts/authContext';
 
-const router = createRouter({ routeTree, context: { auth: undefined! } });
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  context: { auth: undefined!, queryClient },
+  defaultPreload: 'intent',
+  defaultPreloadStaleTime: 0,
+});
 
 declare module '@tanstack/react-router' {
-	interface Register {
-		router: typeof router;
-	}
+  interface Register {
+    router: typeof router;
+  }
 }
 
 function InnerApp() {
-	const auth = useAuth();
-	return <RouterProvider router={router} context={{ auth }} />;
+  const auth = useAuth();
+  return <RouterProvider router={router} context={{ auth }} />;
 }
 
-const queryClient = new QueryClient();
-
 function App() {
-	return (
-		<QueryClientProvider client={queryClient}>
-			<AuthProvider>
-				<InnerApp />
-			</AuthProvider>
-		</QueryClientProvider>
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <InnerApp />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;

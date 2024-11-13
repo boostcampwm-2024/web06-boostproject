fde28a09de8a6b015bd3f49da950da7722aa5e49
PMtHk <zooby88@gmail.com>
2024-11-13 17:48:49 +0900

    fix: context 에러 처리 추가

--- apps/client/src/contexts/authContext.tsx
+++ apps/client/src/contexts/authContext.tsx
@@ -50,5 +50,10 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 }
 
 export const useAuth = () => {
-	return useContext(AuthContext);
+	const context = useContext(AuthContext);
+	if (!context) {
+		throw new Error('useAuth must be used within an AuthProvider');
+	}
+
+	return context;
 };

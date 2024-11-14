73efb9699c1d735a189e98b9221db513706e45b2
PMtHk <zooby88@gmail.com>
2024-11-14 16:49:19 +0900

    chore: 누락된 포매팅 적용

--- apps/client/src/routes/_auth.$project.board.tsx
+++ apps/client/src/routes/_auth.$project.board.tsx
@@ -2,5 +2,5 @@ import { createFileRoute } from '@tanstack/react-router';
 import Kanban from '@/pages/Kanban.tsx';
 
 export const Route = createFileRoute('/_auth/$project/board')({
-	component: Kanban,
+  component: Kanban,
 });

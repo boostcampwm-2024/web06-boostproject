db5174e31c7bc29692ba21e8ea5b6774840efa85
jjeonghak <rlawjdgks3185@khu.ac.kr>
2024-11-14 18:30:13 +0900

    feat: 프로젝트 생성시 기본 섹션 생성

--- apps/server/src/project/project.module.ts
+++ apps/server/src/project/project.module.ts
@@ -7,9 +7,10 @@ import { Contributor } from './entity/contributor.entity';
 import { Account } from '@/account/entity/account.entity';
 import { Task } from '@/task/domain/task.entity';
 import { ProjectsController } from '@/project/controller/projects.controller';
+import { Section } from '@/task/domain/section.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project, Contributor, Account, Task])],
+  imports: [TypeOrmModule.forFeature([Project, Contributor, Account, Task, Section])],
   controllers: [ProjectController, ProjectsController],
   providers: [ProjectService],
 })

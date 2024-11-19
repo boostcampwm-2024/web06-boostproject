1e9a27a69ed508925e05dd140aa082e2409f9454
PMtHk <zooby88@gmail.com>
2024-11-19 21:37:39 +0900

    feat: 태스크 이동 기능 추가

--- apps/client/src/components/Kanban.tsx
+++ apps/client/src/components/Kanban.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { DragEvent, useState } from 'react';
 import { useParams } from '@tanstack/react-router';
 import { useMutation, UseQueryResult } from '@tanstack/react-query';
 import { PlusIcon } from '@radix-ui/react-icons';
@@ -63,6 +63,111 @@ export default function Kanban({ sections, refetch }: KanbanProps) {
     createTaskMutation.mutate({ sectionId, position });
   };
 
+  // drag&drop
+  const [activeSectionId, setActiveSectionId] = useState(-1);
+  const [belowTaskId, setBelowTaskId] = useState(-1);
+
+  const { mutate: updateTaskPosition } = useMutation({
+    mutationFn: async ({
+      sectionId,
+      taskId,
+      position,
+    }: {
+      sectionId: number;
+      taskId: number;
+      position: string;
+    }) => {
+      const payload = {
+        event: 'UPDATE_POSITION',
+        sectionId,
+        taskId,
+        position,
+      };
+
+      return axios.post(`/api/project/${projectId}/update`, payload, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+    },
+    onSuccess: async () => {
+      setActiveSectionId(-1);
+      setBelowTaskId(-1);
+      await refetch();
+    },
+    onError: (error) => {
+      console.error('Failed to update task position:', error);
+      setDialogError('태스크 이동 중 문제가 발생했습니다. 다시 시도해주세요.');
+      setActiveSectionId(-1);
+      setActiveSectionId(-1);
+    },
+  });
+
+  const handleDragOver = (e: DragEvent<HTMLDivElement>, sectionId: number, taskId?: number) => {
+    e.preventDefault();
+    setActiveSectionId(sectionId);
+
+    if (!taskId) {
+      setBelowTaskId(-1);
+      return;
+    }
+
+    setBelowTaskId(taskId);
+  };
+
+  const handleDragLeave = () => {
+    setBelowTaskId(-1);
+    setActiveSectionId(-1);
+  };
+
+  const handleDrop = (event: DragEvent<HTMLDivElement>, sectionId: number) => {
+    const taskId = parseInt(event.dataTransfer.getData('taskId'), 10);
+
+    if (taskId === belowTaskId) {
+      setActiveSectionId(-1);
+      setBelowTaskId(-1);
+      return;
+    }
+
+    updateTaskPosition({
+      sectionId,
+      taskId,
+      position: calculatePosition(
+        sections.find((section) => section.id === sectionId)?.tasks || [],
+        belowTaskId
+      ),
+    });
+  };
+
+  const calculatePosition = (tasks: TTask[], belowTaskId: number) => {
+    if (tasks.length === 0) {
+      // 빈 섹션이라면 랜덤 값 부여.
+      return LexoRank.middle().toString();
+    }
+
+    if (belowTaskId === -1) {
+      // 특정 태스크 위에 드랍하지 않은 경우
+      return LexoRank.parse(tasks[tasks.length - 1].position)
+        .genNext()
+        .toString();
+    }
+
+    const belowTaskIndex = tasks.findIndex((task) => task.id === belowTaskId);
+    const belowTask = tasks[belowTaskIndex];
+
+    if (belowTaskIndex === 0) {
+      // 첫 번째 태스크 위에 드랍한 경우
+      return LexoRank.parse(belowTask.position).genPrev().toString();
+    }
+
+    return LexoRank.parse(tasks[belowTaskIndex - 1].position)
+      .between(LexoRank.parse(belowTask.position))
+      .toString();
+  };
+
+  const handleDragStart = (event: DragEvent<HTMLDivElement>, sectionId: number, task: TTask) => {
+    event.dataTransfer.setData('taskId', task.id.toString());
+    event.dataTransfer.setData('sectionId', sectionId.toString());
+  };
+
   return (
     <>
       <div className="flex h-[calc(100vh-110px)] flex-1 flex-row space-x-2 overflow-x-auto p-4">
@@ -72,9 +177,29 @@ export default function Kanban({ sections, refetch }: KanbanProps) {
             section={section}
             onCreateTask={handleCreateTask}
             isCreatingTask={createTaskMutation.isPending}
+            onDragOver={(e) =>
+              handleDragOver(e as unknown as DragEvent<HTMLDivElement>, section.id)
+            }
+            onDragLeave={handleDragLeave}
+            onDrop={(e) => handleDrop(e as unknown as DragEvent<HTMLDivElement>, section.id)}
+            className={
+              activeSectionId === section.id
+                ? 'border-2 border-blue-500'
+                : 'border-2 border-transparent'
+            }
           >
             {section.tasks.map((task) => (
-              <KanbanTask key={task.id} task={task} />
+              <KanbanTask
+                handleDragLeave={handleDragLeave}
+                handleDragOver={(e) =>
+                  handleDragOver(e as unknown as DragEvent<HTMLDivElement>, section.id, task.id)
+                }
+                handleDragStart={(e) =>
+                  handleDragStart(e as unknown as DragEvent<HTMLDivElement>, section.id, task)
+                }
+                key={task.id}
+                task={task}
+              />
             ))}
           </KanbanSection>
         ))}

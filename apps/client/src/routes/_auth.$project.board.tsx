c91b00e78fbc1d07987a1718d3a056c007047f23
PMtHk <zooby88@gmail.com>
2024-11-21 21:16:56 +0900

    chore: TasksResponse 오타 수정

--- apps/client/src/routes/_auth.$project.board.tsx
+++ apps/client/src/routes/_auth.$project.board.tsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { createFileRoute } from '@tanstack/react-router';
 import Board from '@/pages/Board.tsx';
-import { TasksResponseDto } from '@/components/KanbanBoard.tsx';
+import { TasksResponse } from '@/components/KanbanBoard.tsx';
 
 export const Route = createFileRoute('/_auth/$project/board')({
   loader: ({ context: { auth, queryClient }, params: { project: projectId } }) => {
     queryClient.ensureQueryData({
       queryKey: ['tasks', projectId],
       queryFn: async () => {
-        const response = await axios.get<TasksResponseDto>(`/api/task?projectId=${projectId}`, {
+        const response = await axios.get<TasksResponse>(`/api/task?projectId=${projectId}`, {
           headers: { Authorization: `Bearer ${auth.accessToken}` },
         });
 

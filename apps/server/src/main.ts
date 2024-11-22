042973f9599e75508126ef16f865b0ebf08e7ee4
yang <hc1489@naver.com>
2024-11-22 23:22:11 +0900

    chore: Websocket Cors 설정

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 import { AppModule } from '@/app.module';
+import { CustomIoAdapter } from './common/custom-io-adapter';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new CustomIoAdapter(app));
   await app.listen(3000);
 }
 bootstrap();

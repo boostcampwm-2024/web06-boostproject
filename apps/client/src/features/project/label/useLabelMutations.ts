802e6f53709d95345ce6a0d0ac06c53673c892c5
PMtHk <zooby88@gmail.com>
2024-11-26 14:01:35 +0900

    feat: useLabelMutations에 제너릭 타입 추가

--- apps/client/src/features/project/label/useLabelMutations.ts
+++ apps/client/src/features/project/label/useLabelMutations.ts
@@ -1,6 +1,8 @@
+import { AxiosError } from 'axios';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { projectAPI } from '@/features/project/api.ts';
 import { CreateLabelDto, UpdateLabelDto } from '@/features/project/types.ts';
+import { BaseResponse } from '@/features/types.ts';
 
 export const useLabelMutations = (projectId: number) => {
   const queryCleint = useQueryClient();
@@ -12,25 +14,25 @@ export const useLabelMutations = (projectId: number) => {
   };
 
   return {
-    create: useMutation({
-      mutationFn: (createLabelDto: CreateLabelDto) =>
-        projectAPI.createLabel(projectId, createLabelDto),
+    create: useMutation<BaseResponse, AxiosError, CreateLabelDto>({
+      mutationFn: (createLabelDto) => projectAPI.createLabel(projectId, createLabelDto),
       onSuccess: invalidateLabels,
     }),
 
-    update: useMutation({
-      mutationFn: ({
-        labelId,
-        updateLabelDto,
-      }: {
+    update: useMutation<
+      BaseResponse,
+      AxiosError,
+      {
         labelId: number;
         updateLabelDto: UpdateLabelDto;
-      }) => projectAPI.updateLabel(labelId, updateLabelDto),
+      }
+    >({
+      mutationFn: ({ labelId, updateLabelDto }) => projectAPI.updateLabel(labelId, updateLabelDto),
       onSuccess: invalidateLabels,
     }),
 
-    delete: useMutation({
-      mutationFn: (labelId: number) => projectAPI.deleteLabel(labelId),
+    delete: useMutation<BaseResponse, AxiosError, number>({
+      mutationFn: (labelId) => projectAPI.deleteLabel(labelId),
       onSuccess: invalidateLabels,
     }),
   };

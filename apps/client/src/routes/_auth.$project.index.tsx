9ab6e6c7f684959b84d586446ab8bc3496d90b1e
iam454 <realseogy@gmail.com>
2024-11-28 01:03:09 +0900

    fix: 이름 오류 수정

--- apps/client/src/routes/_auth.$project.index.tsx
+++ apps/client/src/routes/_auth.$project.index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router';
-import ProjectOverview from '@/pages/ProjectOVerview';
+import ProjectOverview from '@/pages/ProjectOverview';
 
 export const Route = createFileRoute('/_auth/$project/')({
   component: ProjectOverview,

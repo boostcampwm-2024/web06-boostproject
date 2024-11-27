d80bc5dfa04dfabbf95b0bfdeb1fa6a0ae6a3e38
yang <hc1489@naver.com>
2024-11-27 11:57:43 +0900

    fix: ImageModule에서 UserService import 설정

--- apps/server/src/image/image.module.ts
+++ apps/server/src/image/image.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ImageController } from './controller/image.controller';
 import { ImageService } from './service/image.service';
+import { AccountModule } from '@/account/account.module';
 
 @Module({
   controllers: [ImageController],
   providers: [ImageService],
+  imports: [AccountModule],
 })
 export class ImageModule {}

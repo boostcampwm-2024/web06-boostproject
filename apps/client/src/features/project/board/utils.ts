ed4b40a28d56cf658b2f6294ec75e9ecc1358e7e
PMtHk <zooby88@gmail.com>
2024-11-27 22:43:32 +0900

    fix: title delete 안되는 문제 수정

--- apps/client/src/features/project/board/utils.ts
+++ apps/client/src/features/project/board/utils.ts
@@ -27,3 +27,39 @@ export const calculatePosition = (tasks: Task[], belowTaskId: number): string =>
 
 export const findTask = (sections: TSection[], taskId: number): Task | undefined =>
   sections.flatMap((section) => section.tasks).find((task) => task.id === taskId);
+
+export const findDiff = (str1: string, str2: string) => {
+  let startIndex = 0;
+  let endIndex1 = 0;
+  let endIndex2 = 0;
+
+  while (
+    startIndex < str1.length &&
+    startIndex < str2.length &&
+    str1[startIndex] === str2[startIndex]
+  ) {
+    startIndex += 1;
+  }
+
+  let backIndex1 = str1.length - 1;
+  let backIndex2 = str2.length - 1;
+
+  while (
+    backIndex1 >= startIndex &&
+    backIndex2 >= startIndex &&
+    str1[backIndex1] === str2[backIndex2]
+  ) {
+    backIndex1 -= 1;
+    backIndex2 -= 1;
+  }
+
+  endIndex1 = backIndex1 + 1;
+  endIndex2 = backIndex2 + 1;
+
+  return {
+    position: startIndex,
+    originalContent: str1.substring(startIndex, endIndex1),
+    content: str2.substring(startIndex, endIndex2),
+    length: str2.substring(startIndex, endIndex2).length,
+  };
+};

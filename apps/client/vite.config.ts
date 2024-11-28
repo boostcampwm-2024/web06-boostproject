017c88df3b0cf494a325f001523542efda5db8f5
PMtHk <zooby88@gmail.com>
2024-11-28 15:58:14 +0900

    chore: VITE 환경설정 수정

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/api/socket.io': {
+        target: 'http://localhost:3000',
         ws: true,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),

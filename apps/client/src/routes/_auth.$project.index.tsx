f48e5a41fcdc4b1591f6325272668af802dfabc4
iam454 <realseogy@gmail.com>
2024-12-02 19:41:56 +0900

    refactor: 각각의 쿼리에 대해 useSuspenseQuries로 수정

--- apps/client/src/routes/_auth.$project.index.tsx
+++ apps/client/src/routes/_auth.$project.index.tsx
@@ -1,6 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { Suspense } from 'react';
 import ProjectOverview from '@/pages/ProjectOverview';
 
 export const Route = createFileRoute('/_auth/$project/')({
-  component: ProjectOverview,
+  component: () => (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ProjectOverview />
+    </Suspense>
+  ),
+  errorComponent: () => (
+    <div>
+      <h2>Failed to load overview</h2>
+      <p>Sorry, an unexpected error occured.</p>
+    </div>
+  ),
 });
